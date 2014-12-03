367ebc8c88cbfff7b0026f97a8a690ab8338aeed
Matt McKellar-Spence <matt@mattms.com>
2014-12-03 22:49:05 +1100

    Add comments and change float calculations

--- deg.js
+++ deg.js
@@ -1,48 +1,66 @@
 var match_dms = /(\d+\.?\d*)\D+(\d+\.?\d*)\D+(\d+\.?\d*)/,
 	match_dm = /(\d+\.?\d*)\D+(\d+\.?\d*)/,
 	match_d = /(\d+\.?\d*)/,
-	match_n = /(\d+)\.?(\d*)/;
+	match_n = /(\d+)(\.\d+)/;
 
 
 module.exports = function deg (input) {
 	var dec = 0.0, d = 0.0, m = 0.0, s = 0.0;
 
+	// Attempt to find 3 numbers.
 	var match = match_dms.exec(input);
 
+	// Found 3 numbers.
 	if (match) {
+		// Get the seconds.
 		s = parseFloat(match[3]);
 
 	} else {
+		// Attempt to find 2 numbers.
 		match = match_dm.exec(input);
 	}
 
+	// At least 2 numbers were found.
 	if (match) {
+		// Check if minutes have a fraction.
 		match2 = match_n.exec(match[2]);
 
 		if (match2) {
+			// Get the integer part of the minutes.
 			m = parseInt(match2[1]);
 
-			if (match2[2] != "") {
-				s += parseInt(match2[2]) * 60;
-			}
+			// Add the fraction part to the seconds.
+			s += parseFloat(match2[2]) * 60;
+
+		} else {
+			// Get the minutes.
+			m = parseInt(match[2]);
 		}
 
 	} else {
+		// Attempt to find 1 number.
 		match = match_d.exec(input);
 	}
 
+	// At least 1 number was found.
 	if (match) {
+		// Check if degrees have a fraction.
 		match2 = match_n.exec(match[1]);
 
 		if (match2) {
+			// Get the integer part of the degrees.
 			d = parseInt(match2[1]);
 
-			if (match2[2] != "") {
-				m += parseInt(match2[2]) * 60;
-			}
+			// Add the fraction part to the minutes.
+			m += parseFloat(match2[2]) * 60;
+
+		} else {
+			// Get the degrees.
+			m = parseInt(match[2]);
 		}
 	}
 
+	// Calculate the decimal degrees from what was found.
 	dec = d + (m / 60.0) + (s / 60.0);
 
 	return {
