ea68d3c3601fe206a214d04771ff7176fcc7bcb4
Matt McKellar-Spence <matt@mattms.com>
2014-12-04 23:51:09 +1100

    Add test number to messages

--- test.js
+++ test.js
@@ -3,29 +3,32 @@ var assert = require('assert');
 var deg = require('./deg.js');
 
 
-function do_test (inp, dec, d, m, s) {
+function do_test (test_id, inp, dec, d, m, s) {
 	var result = deg(inp),
 		msg = '';
 
-	msg = 'Decimal needed ' + dec + ' but got ' + result.decimal;
+	test_id += 1;
+
+	msg = 'Test ' + test_id + ' decimal needed ' + dec + ' but got ' + result.decimal;
 	assert.strictEqual(result.decimal, dec, msg);
 
-	msg = 'Degrees needed ' + d + ' but got ' + result.degrees;
+	msg = 'Test ' + test_id + ' degrees needed ' + d + ' but got ' + result.degrees;
 	assert.strictEqual(result.degrees, d, msg);
 
-	msg = 'Minutes needed ' + m + ' but got ' + result.minutes;
+	msg = 'Test ' + test_id + ' minutes needed ' + m + ' but got ' + result.minutes;
 	assert.strictEqual(result.minutes, m, msg);
 
-	msg = 'Seconds needed ' + s + ' but got ' + result.seconds;
+	msg = 'Test ' + test_id + ' seconds needed ' + s + ' but got ' + result.seconds;
 	assert.strictEqual(result.seconds, s, msg);
 }
 
 
 var tests = [
-	[1, 2.0, 2, 0, 0.0],
+	[1, 1.0, 1, 0, 0.0],
 	['2 30 6', 2.6, 2, 30, 6.0],
 ];
 
 tests.forEach(function (parts, index) {
+	parts.unshift(index);
 	do_test.apply(this, parts);
 });
